974f2f40a741088c9bd164efff19d73ffdd5df47
evgeny_t <timoshenko.evgeny@gmail.com>
2014-04-16 17:27:45 +0700

    add empty SelectDateWidget

---
+++ QDateRange/selectdatewidget.h
@@ -0,0 +1,18 @@
+#ifndef SELECTDATEWIDGET_H
+#define SELECTDATEWIDGET_H
+
+#include <QWidget>
+
+class SelectDateWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SelectDateWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // SELECTDATEWIDGET_H
